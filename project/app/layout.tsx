2e5284a42d0acc11f401e33b049b9cb22a79ac07
remendoza-strat <i_rhabiezra.mendoza@stratpoint.com>
2025-07-25 12:36:13 +0800

    feat: installed zod and sonner

--- project/app/layout.tsx
+++ project/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "sonner"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider>{children}</ThemeProvider>
+        <Toaster richColors position="bottom-right" />
       </body>
     </html>
     </ClerkProvider>
